591e209ca2da8efc591239bbebffd98e8b8846bc
kavabunga <kavabunga@gmail.com>
2022-11-21 13:22:07 +0200

    fix: PR10 review 02

--- src/components/PopupImage.js
+++ src/components/PopupImage.js
@@ -2,8 +2,8 @@ export default function ImagePopup({card, onClose}) {
   return (
     <div className={`popup popup_type_preview ${card && 'popup_opened'}`}>
       <div className="popup__preview-container">
-        <img src={card && card.link} alt={card && card.name} className="popup__image" />
-        <h2 className="popup__caption">{card && card.name}</h2>
+        <img src={card?.link} alt={card?.name} className="popup__image" />
+        <h2 className="popup__caption">{card?.name}</h2>
         <button type="button" className="popup__close-button" onClick={onClose}/>
       </div>
     </div>
